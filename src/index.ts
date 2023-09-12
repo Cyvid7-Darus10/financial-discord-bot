47aa502edbb5361ee32f259eb8a06c694846179d
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2023-09-12 21:34:31 +0800

    chore(.prettierignore): add .env and /dist to .prettierignore to exclude them from formatting chore(.prettierrc.json): add .prettierrc.json file with prettier configuration chore(package.json): add prettier as a dev dependency style(index.ts): change import statements to use single quotes instead of double quotes for consistency style(index.ts): change intents array to use single quotes instead of double quotes for consistency style(index.ts): remove unnecessary semicolons for consistency

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import { Client } from "discord.js";
-import { config } from "dotenv";
+import { Client } from 'discord.js'
+import { config } from 'dotenv'
 
-config();
+config()
 
 const client: Client = new Client({
     intents: [
-        "Guilds",
-        "GuildMessages",
-        "GuildMessageReactions",
-        "GuildVoiceStates",
+        'Guilds',
+        'GuildMessages',
+        'GuildMessageReactions',
+        'GuildVoiceStates',
     ],
-});
+})
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user?.tag}!`);
-});
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user?.tag}!`)
+})
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN)
