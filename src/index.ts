20926e4b700e929dbf21b1139ee35f5b84c51764
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2023-09-12 21:48:50 +0800

    chore(index.js): update intents array to use single quotes for consistency feat(index.js): add command handling for GPT bot command chore(index.ts): update intents array to use single quotes for consistency feat(index.ts): add command handling for GPT bot command

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,26 @@ const client: Client = new Client({
         'GuildMessages',
         'GuildMessageReactions',
         'GuildVoiceStates',
+        'MessageContent',
     ],
 })
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user?.tag}!`)
+})
+
+const IGNORE_PREFIX = '!'
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) return
+    if (!message.content.startsWith(IGNORE_PREFIX)) return
+
+    const args = message.content.slice(IGNORE_PREFIX.length).trim().split(/ +/g)
+    const command = args.shift()?.toLowerCase()
+
+    if (command === 'gpt') {
+        message.reply('Hello, world!, I am GPT Bot, I am here to help you!')
+    }
 })
 
 client.login(process.env.BOT_TOKEN)
