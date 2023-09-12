4ce0367d2513993f2419bbcc1a1184847e26edc7
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2023-09-12 22:12:52 +0800

    fix(index.ts): remove unnecessary intent 'GuildMessageReactions' from client intents feat(index.ts): add support for OpenAI API integration to enable AI chatbot functionality fix(index.ts): remove unnecessary intent 'GuildMessageReactions' from client intents feat(index.ts): add support for OpenAI API integration to enable AI chatbot functionality fix(server.ts): change port variable case from lowercase port to uppercase PORT to improve semantics feat(server.ts): add support for process.env.PORT environment variable to be able to run app on a configurable port

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,59 @@
 import { Client, Message } from 'discord.js'
 import { config } from 'dotenv'
+import OpenAI from 'openai'
 
 config()
 
 const client: Client = new Client({
-    intents: [
-        'Guilds',
-        'GuildMessages',
-        'GuildMessageReactions',
-        'GuildVoiceStates',
-        'MessageContent',
-    ],
+    intents: ['Guilds', 'GuildMessages', 'MessageContent'],
 })
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user?.tag}!`)
 })
 
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
 const IGNORE_PREFIX = '!'
 
-client.on('messageCreate', (message: Message) => {
+client.on('messageCreate', async (message: Message) => {
     if (message.author.bot) return
     if (!message.content.startsWith(IGNORE_PREFIX)) return
 
-    const args = message.content.slice(IGNORE_PREFIX.length).trim().split(/ +/g)
-    const command = args.shift()?.toLowerCase()
+    const msgContent = message.content
+    const command: string = msgContent.split(' ')[0].toLowerCase()
+    const prompt: string = msgContent.slice(command.length + 1).toLowerCase()
 
-    if (command === 'gpt') {
-        message.reply('Hello, world!, I am GPT Bot, I am here to help you!')
+    if (command === '!gpt') {
+        const response = await openai.chat.completions
+            .create({
+                model: 'gpt-4',
+                messages: [
+                    {
+                        role: 'system',
+                        content:
+                            'The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.',
+                    },
+                    {
+                        role: 'user',
+                        content: prompt,
+                    },
+                ],
+            })
+            .catch((err) => {
+                console.error("Couldn't create completion", err)
+            })
+            .finally(() => {
+                console.log('Completed')
+            })
+
+        const responseText = response?.choices[0]?.message?.content
+
+        if (responseText) {
+            message.channel.send(responseText)
+        }
     }
 })
 
