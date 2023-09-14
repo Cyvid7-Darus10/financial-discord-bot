399a0c62422d6d22ee0a844ce20828c03cdde6c3
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2023-09-14 21:19:34 +0800

    fix(roleCreate.ts): fix event name in comments from 'roleDelete' to 'roleCreate' for accuracy feat(roleCreate.ts): update comments to provide clearer explanation of the purpose of the 'roleCreate' event

--- src/discord/events/role/roleCreate.ts
+++ src/discord/events/role/roleCreate.ts
@@ -1,27 +1,28 @@
 import { Event } from '../../structures/Event';
 
 /**
- * Event module for the 'roleDelete' event. This event is triggered whenever
- * a role within a guild is deleted.
+ * Event module for the 'roleCreate' event. This event is triggered whenever
+ * a new role within a guild is created.
  *
- * Redeploying commands after a role is deleted ensures that any command permissions
- * or dependencies related to the deleted role are updated and remain consistent
- * within the bot's command access controls.
+ * Redeploying commands after a role is created ensures that the bot's commands
+ * remain consistent and up-to-date with regards to any new roles, especially if
+ * some commands have specific interactions, permissions, or dependencies related
+ * to the newly created role.
  */
 export default new Event({
-    // The event's name, 'roleDelete', signifies that a role within the guild has been deleted.
-    name: 'roleDelete',
+    // The event's name, 'roleCreate', signifies that a new role within the guild has been created.
+    name: 'roleCreate',
 
     /**
-     * The main function that gets executed when the 'roleDelete' event is triggered.
+     * The main function that gets executed when the 'roleCreate' event is triggered.
      *
      * @param {GPTBotClient} client - The instance of our custom Discord bot client.
      */
     run: async (client) => {
-        // Redeploy the bot's commands when a role is deleted.
-        // This ensures that command access controls remain consistent, especially
-        // for any commands that might have had specific permissions or dependencies
-        // related to the deleted role.
+        // Redeploy the bot's commands when a new role is created.
+        // This ensures that command interactions, permissions, or dependencies
+        // related to the newly created role are taken into account and the bot's
+        // command access controls remain up-to-date.
         await client.deployCommands();
     },
 });
