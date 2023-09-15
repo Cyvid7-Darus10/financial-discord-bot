fd1931b03f18dea9495fe9f41ba0c6856ddb95fe
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2023-09-16 02:40:54 +0800

    feat: created the plaid dashboard for testing

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-import { config as dotenvConfig } from 'dotenv';
-dotenvConfig();
-
-export const BOT_TOKEN = process.env.BOT_TOKEN;
-export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-export const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
-export const PLAID_SECRET = process.env.PLAID_SECRET;
